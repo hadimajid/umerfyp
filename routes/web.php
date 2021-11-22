144005c2d57452ba1591cfcae3d02e09dfc3963c
hadimajidglowerx <hadi.majid@glowerx.com>
2021-11-22 22:04:53 +0500

    test changes

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/optimize', 'FrontController@optimize');
+
 Route::get('/my-dashboard/credit/deposit', 'UserDashboardController@deposit')->name('userdash.creditdep');
 Route::post('/my-dashboard/credit/deposit', 'UserDashboardController@depositamount')->name('userdash.creditdepamount');
 Route::post('/my-dashboard/credit/deposit/success/{amount}', 'UserDashboardController@depositamountsuccess')->name('userdash.creditdepositedsuccess');
